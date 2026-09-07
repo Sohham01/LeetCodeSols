21db67e0e8aa8e81128925ed690ada685f261e41
Sohham Choudhary <bsdsai25sohhamc@iimsambalpur.ac.in>
2026-09-07 14:26:39 +0530

    Time: 3 ms (75.18%) | Memory: 19.4 MB (13.29%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -0,0 +1,29 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        rows,cols=len(grid),len(grid[0])
+        if rows==0:
+            return -1
+        cnt=0
+        rotten=deque()
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]==2:
+                    rotten.append((r,c))
+                elif grid[r][c]==1:
+                    cnt+=1
+        mins=0
+        dirc=[(1,0),(-1,0),(0,1),(0,-1)]
+        while rotten and cnt>0:
+            mins+=1
+            for i in range(len(rotten)):
+                r,c=rotten.popleft()
+                for dr,dc in dirc:
+                    row,col=r+dr,c+dc
+                    if row<0 or row==rows or col<0 or col==cols:
+                        continue
+                    if grid[row][col]==0 or grid[row][col]==2:
+                        continue
+                    cnt-=1
+                    grid[row][col]=2
+                    rotten.append((row,col))
+        return mins if cnt==0 else -1
