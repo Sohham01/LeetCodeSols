f1c34bdb06029097b117b17d47ba93304b3d85c3
Sohham Choudhary <bsdsai25sohhamc@iimsambalpur.ac.in>
2026-09-17 13:43:23 +0530

    Time: 263 ms (54.00%) | Memory: 22 MB (25.16%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.py
@@ -4,18 +4,18 @@ class Solution:
             return 0
         rows,cols=len(heights),len(heights[0])
         heap=[(0,0,0)]
-        max_eff=0
         visited=set()
         dirn=[(1,0),(-1,0),(0,1),(0,-1)]
         while heap:
             eff,r,c=heapq.heappop(heap)
-            max_eff=max(max_eff,eff) 
             if (r,c)==(rows-1,cols-1):
-                return max_eff
+                return eff
+            if (r,c) in visited:
+                continue
             visited.add((r,c))
             for dr,dc in dirn:
                 rr,cc=r+dr,c+dc
                 if 0<=rr<rows and 0<=cc<cols and (rr,cc) not in visited:
-                    eff=abs(heights[rr][cc]-heights[r][c])
-                    heapq.heappush(heap,(eff,rr,cc))
-        return max_eff
+                    diff=abs(heights[rr][cc]-heights[r][c])
+                    new_eff=max(eff,diff)
+                    heapq.heappush(heap,(new_eff,rr,cc))
